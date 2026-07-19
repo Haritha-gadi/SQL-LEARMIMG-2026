c0d5f2b22b13a3aae799f061042c61e525426bf4
Haritha <gadiharitha2730@gmail.com>
2026-07-19 15:07:17 +0530

    Add SQL Day 2 - Database, Table, Insert and Select

---
+++ Day-02/03_create_table.sql
@@ -0,0 +1,6 @@
+CREATE TABLE Employee (
+    Emp_ID INT,
+    Emp_Name VARCHAR(50),
+    Department VARCHAR(50),
+    Salary INT
+);
---
+++ Day-02/04_insert_data.sql
@@ -0,0 +1,6 @@
+INSERT INTO Employee VALUES
+(101, 'Rahul', 'HR', 30000),
+(102, 'Priya', 'IT', 45000),
+(103, 'Kiran', 'Finance', 40000),
+(104, 'Sneha', 'Marketing', 35000),
+(105, 'Arjun', 'IT', 50000);
